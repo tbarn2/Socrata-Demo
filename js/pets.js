653f9d5dd79c61b6747b89bbbff47bbb0c62f1cb
adamsebetich@yahoo.com <adamsebetich@yahoo.com>
2015-11-21 14:28:53 -0800

    updated

--- js/pets.js
+++ js/pets.js
@@ -2,7 +2,7 @@
 angular.module('PetApp', [])
 .controller('PetCtrl', ['$scope', '$http', function($scope, $http) {
 
-  $http.get('https://data.kingcounty.gov/resource/murn-chih.json').then(function(response){
+  $http.get('https://data.kingcounty.gov/resource/murn-chih.json?record_type=FOUND').then(function(response){
     		console.log(response.data);
     		$scope.pets = response.data;
 	});
